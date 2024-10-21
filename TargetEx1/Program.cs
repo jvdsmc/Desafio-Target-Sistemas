7abde50f41e3127395b5fc28d4217b87d47d2361
jvdsmc <joaovictormpl@gmail.com>
2024-10-21 18:46:26 -0300

    Add project files.

---
+++ TargetEx1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace TargetEx1
+{
+    internal class Program
+    {
+        static bool seqFibonacci(int num)
+        {
+            int a = 0;
+            int b = 1;
+
+            while (b < num)
+            {
+                int n = a;
+                a = b;
+                b = n + b;
+            }
+            return b == num || num == 0;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("Digite um número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (seqFibonacci(numero))
+            {
+                Console.WriteLine($"O número {numero} pertence a sequência de Fibonacci.");
+            } 
+            else
+            {
+                Console.WriteLine($"O número {numero} não pertence a sequência de Fibonacci.");
+            }
+        }
+    }
+}
---
+++ TargetEx2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace TargetEx2
+{
+    internal class Program
+    {
+        static int ContadorA(string input)
+        {
+            int contador = 0;
+
+            foreach (char a in input)
+            {
+                if (a == 'a' || a == 'A')
+                {
+                    contador++;
+                }
+            }
+            return contador;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a string a ser analisada: ");
+            string input = Console.ReadLine();
+
+            int contador = ContadorA(input);
+            Console.WriteLine($"Na string {input}, a letra A aparece {contador} vezes.");
+        }
+    }
+}
